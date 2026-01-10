3aaf0d9cc571a264530168834576af11b32562ef
Dominik Šimoník <dominik.simonik@gmail.com>
2026-01-10 23:59:35 +0100

    chore: small observing changes

---
+++ lib/app/app_bloc_observer.dart
@@ -0,0 +1,47 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
+/// Observes all bloc and cubit activity in the application.
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    super.onCreate(bloc);
+    log('onCreate: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('onEvent: ${bloc.runtimeType} $event');
+  }
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange: ${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    log('onTransition: ${bloc.runtimeType} $transition');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    log('onError: ${bloc.runtimeType} $error $stackTrace');
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    super.onClose(bloc);
+    log('onClose: ${bloc.runtimeType}');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:nostr_place/app/app.dart';
+import 'package:nostr_place/app/app_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = const AppBlocObserver();
   runApp(const App());
 }
